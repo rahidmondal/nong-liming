276d8b4277667bf41c542e71845abb1cc8d7d042
Rahid Mondal <71984696+rahidmondal@users.noreply.github.com>
2026-02-15 20:04:17 +0530

    feat: improve PWA - with user input based update and install + change cache statergy

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
 import App from '@/App';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { registerSW } from 'virtual:pwa-register';
 import './index.css';
-
-registerSW({ immediate: true });
 
 const rootElement = document.getElementById('root');
 
